c64b41927357ca96ec8eaab7b46256528a3d3281
Zachary Juang <zachary822@me.com>
2022-05-03 04:36:56 -0400

    Add yaml openapi spec

--- api/main.py
+++ api/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, Request, status
+import yaml
+from fastapi import FastAPI, Request, Response, status
+from fastapi.concurrency import run_in_threadpool
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from gridfs.errors import NoFile
@@ -25,6 +27,17 @@ app.include_router(health.router, prefix="/health")
 def handle_gridfs_file_not_found(_request: Request, _exc: NoFile):
     return JSONResponse(
         status_code=status.HTTP_404_NOT_FOUND, content={"detail": "image not found"}
+    )
+
+
+@app.get("/openapi.yaml")
+async def read_openapi_yaml() -> Response:
+    openapi_json = app.openapi()
+    return Response(
+        await run_in_threadpool(
+            yaml.dump, openapi_json, sort_keys=False, Dumper=yaml.Dumper
+        ),
+        media_type="text/yaml",
     )
 
 
