48c0f9437200c9d4c91130a485dc1b363b026f55
Zachary Juang <zachary822@me.com>
2022-05-02 23:52:11 -0400

    More better image handling

--- api/images/__init__.py
+++ api/images/__init__.py
@@ -1,4 +1,3 @@
-import pendulum
 from fastapi import APIRouter, Depends
 from fastapi.responses import Response, StreamingResponse
 from motor.motor_asyncio import AsyncIOMotorGridFSBucket
@@ -6,7 +5,6 @@ from motor.motor_asyncio import AsyncIOMotorGridFSBucket
 from api.dependencies import get_fs
 from api.images import crud
 from api.types import ObjectId
-from api.utils import to_rfc7231_format
 
 router = APIRouter(tags=["images"])
 
@@ -16,14 +14,10 @@ async def get_image(
     object_id: ObjectId, fs: AsyncIOMotorGridFSBucket = Depends(get_fs)
 ):
     grid_out = await crud.get_image(fs, object_id)
-    last_modified = pendulum.instance(grid_out.upload_date)
     return StreamingResponse(
         crud.grid_iter(grid_out),
         media_type=grid_out.content_type,
-        headers={
-            "Content-Length": str(grid_out.length),
-            "Last-Modified": to_rfc7231_format(last_modified),
-        },
+        headers=crud.get_image_headers(grid_out),
     )
 
 
@@ -32,11 +26,7 @@ async def get_image_headers(
     object_id: ObjectId, fs: AsyncIOMotorGridFSBucket = Depends(get_fs)
 ):
     grid_out = await crud.get_image(fs, object_id)
-    last_modified = pendulum.instance(grid_out.upload_date)
     return Response(
         media_type=grid_out.content_type,
-        headers={
-            "Content-Length": str(grid_out.length),
-            "Last-Modified": to_rfc7231_format(last_modified),
-        },
+        headers=crud.get_image_headers(grid_out),
     )
--- api/images/crud.py
+++ api/images/crud.py
@@ -1,13 +1,30 @@
 from typing import AsyncIterator
 
+import pendulum
 from motor.motor_asyncio import AsyncIOMotorGridFSBucket
 from motor.motor_gridfs import AgnosticGridOut
 
 from api.types import ObjectId
+from api.utils import to_rfc7231_format
 
 
 def get_image(fs: AsyncIOMotorGridFSBucket, object_id: ObjectId) -> AgnosticGridOut:
     return fs.open_download_stream(object_id)
+
+
+def get_image_headers(out: AgnosticGridOut):
+    last_modified = pendulum.instance(out.upload_date)
+    headers = {
+        "Content-Length": str(out.length),
+        "Last-Modified": to_rfc7231_format(last_modified),
+    }
+
+    try:
+        headers["ETag"] = f'"{out.metadata["md5"]}"'
+    except KeyError:
+        pass
+
+    return headers
 
 
 async def grid_iter(
