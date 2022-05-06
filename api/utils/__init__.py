39b68dfd8b96dee365cacbf93044c49804dd9dfb
Zachary Juang <zachary822@me.com>
2022-05-05 21:55:58 -0400

    Tweak to_rfc7231_format

--- api/utils/__init__.py
+++ api/utils/__init__.py
@@ -1,5 +1,9 @@
-from pendulum import DateTime
+from typing import Union
+
+import pendulum
 
 
-def to_rfc7231_format(dt: DateTime) -> str:
+def to_rfc7231_format(dt: Union[pendulum.DateTime, str]) -> str:
+    if isinstance(dt, str):
+        dt = pendulum.parse(dt)
     return dt.in_tz("GMT").format("ddd, DD MMM YYYY HH:mm:ss zz")
