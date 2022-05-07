1b6263a48fb520d3bb5e57e200e84e53ab448d01
Zachary Juang <zachary822@me.com>
2022-05-07 14:58:14 -0400

    Better configs, tests

--- api/utils/__init__.py
+++ api/utils/__init__.py
@@ -1,9 +1,16 @@
-from typing import Union
+from functools import singledispatch
 
 import pendulum
 
 
-def to_rfc7231_format(dt: Union[pendulum.DateTime, str]) -> str:
-    if isinstance(dt, str):
-        dt = pendulum.parse(dt)
+@singledispatch
+def to_rfc7231_format(dt: pendulum.DateTime) -> str:
     return dt.in_tz("GMT").format("ddd, DD MMM YYYY HH:mm:ss zz")
+
+
+@to_rfc7231_format.register
+def _(dt: str) -> str:  # type: ignore[misc]
+    dt_obj = pendulum.parse(dt)
+    if not isinstance(dt_obj, pendulum.DateTime):
+        raise ValueError("not datetime")
+    return to_rfc7231_format(dt_obj)
--- tests/routes/test_images.py
+++ tests/routes/test_images.py
@@ -4,27 +4,30 @@ from unittest.mock import AsyncMock, MagicMock
 
 import pytest
 from gridfs.errors import NoFile
-from motor.motor_gridfs import AgnosticGridOut
+from motor.motor_asyncio import AsyncIOMotorGridOut
 
 
 class TestImages:
     @pytest.fixture
     def image_file(self):
-        grid_out = MagicMock(AgnosticGridOut)
-        grid_out.read = AsyncMock(side_effect=chain([b"abc"], repeat(b"")))
-        grid_out.upload_date = datetime.now()
-        grid_out.content_type = "image/png"
-        grid_out.length = 3
-        yield grid_out
+        def wrapper(data: bytes):
+            grid_out = MagicMock(AsyncIOMotorGridOut)
+            grid_out.read = AsyncMock(side_effect=chain([data], repeat(b"")))
+            grid_out.upload_date = datetime.now()
+            grid_out.content_type = "image/png"
+            grid_out.length = 3
+            return grid_out
+
+        return wrapper
 
     def test_get_image_found(self, client, mongodb_fs, image_file):
-        mongodb_fs.open_download_stream = AsyncMock(return_value=image_file)
+        mongodb_fs.open_download_stream = AsyncMock(return_value=image_file(b"abc"))
 
         response = client.get("/images/aaaaaaaaaaaaaaaaaaaaaaaa/")
         assert response.status_code == 200
         assert response.content == b"abc"
 
-    def test_get_image_not_found(self, client, mongodb_fs, image_file):
+    def test_get_image_not_found(self, client, mongodb_fs):
         mongodb_fs.open_download_stream = AsyncMock(side_effect=NoFile)
 
         response = client.get("/images/aaaaaaaaaaaaaaaaaaaaaaaa/")
