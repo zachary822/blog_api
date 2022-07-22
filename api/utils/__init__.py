35664e213fadec1bfed9ca430665a29438f51335
Zachary Juang <zachary822@me.com>
2022-07-21 20:34:32 -0400

    Improve to rfc7321

--- api/utils/__init__.py
+++ api/utils/__init__.py
@@ -4,13 +4,15 @@ import pendulum
 
 
 @singledispatch
-def to_rfc7231_format(dt: pendulum.DateTime) -> str:
+def to_rfc7231_format(dt) -> str:
+    raise NotImplementedError(f"cannot format {dt}")
+
+
+@to_rfc7231_format.register
+def _(dt: pendulum.DateTime):
     return dt.in_tz("GMT").format("ddd, DD MMM YYYY HH:mm:ss zz")
 
 
 @to_rfc7231_format.register
-def _(dt: str) -> str:  # type: ignore[misc]
-    dt_obj = pendulum.parse(dt)
-    if not isinstance(dt_obj, pendulum.DateTime):
-        raise ValueError("not datetime")
-    return to_rfc7231_format(dt_obj)
+def _(dt: str):
+    return to_rfc7231_format(pendulum.parse(dt))
